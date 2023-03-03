e5914279a6ba68f9a794933a7c3e464eb9bd506e
Ivan8a <isantoyo0@ucol.mx>
2023-03-02 20:01:24 -0600

    practica9

---
+++ practica9/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send (`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>
+            OWO
+        </h1>
+        
+    </body>
+    </html>`)
+});
+
+app.listen(3000);
+app.get("/uno", (req, res) => {
+    res.send("Hello world from route one")
+})

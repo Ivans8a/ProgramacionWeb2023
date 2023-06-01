6062f30806a8f1dad3a0d539d0daad8ff3244c4b
Ivans8a <isantoyo@ucol.mx>
2023-06-01 14:05:08 -0600

    correcciones practica20

--- practica20/src/routes/person.js
+++ practica20/src/routes/person.js
@@ -4,26 +4,26 @@ const mongoose=require('mongoose');//requerimiento de mongoose
 let Person= require('../models/person');//requerimiento del modelo de persona
 
 router.get('/gente', async (req, res)=>{    //ruta para obtener todas las personas
-    const Persons = await Person.find({});
-    res.render('person.ejs', { Persons });
+    const Persons = await Person.find({});  //obtener todas las personas
+    res.render('person.ejs', { Persons });  //renderizar la vista de personas
 })
 
 router.get('/addPerson', (req, res)=>{ //ruta para agregar una persona
     res.render('addPerson');
 });
 
-router.post('addPerson.ejs', (req, res)=>{ //ruta para agregar una persona
-    const newPerson = new Person({
+router.post('/addPerson', (req, res)=>{ //ruta para agregar una persona
+    const newPerson = new Person({      //creacion de una nueva persona
         nombre: req.body.nombre,
         edad: req.body.edad,
         tipoSangre: req.body.tipoSangre,
         nss: req.body.nss
     });
 
-    newPerson
-    .save()
-    .then((data)=>{res.redirect('/gente')})
-    .catch((error)=>{res.json({message:error})})
+    newPerson   
+    .save()    //guardar la nueva persona
+    .then((data)=>{res.redirect('/gente')}) //redireccionar a la vista de personas  
+    .catch((error)=>{res.json({message:error})})    //mostrar error
 });
 
-module.exports=router;
+module.exports=router;  //exportar las rutas
